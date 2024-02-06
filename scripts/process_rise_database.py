a4e1322878298a50d98728c45b62890f8f63d097
Jonathan Hodges <johodges@vt.edu>
2024-02-06 16:59:01 -0500

    fix rise filename

--- scripts/process_rise_database.py
+++ scripts/process_rise_database.py
@@ -327,7 +327,7 @@ if __name__ == "__main__":
                     tmp[long_ind] = tmp[long_ind][:-1]
                     mat_name = '_'.join(tmp)
                 if mat in ignores: continue
-                dataFile = os.path.join(out_dir, mat_name+'.csv')
+                dataFile = os.path.join(out_dir, mat_name+'-%0.0f.csv'%(flux))
                 d.to_csv(dataFile, index=False)
     
     material_database = material_database_filtered
