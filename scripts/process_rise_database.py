64046e2ba4cb8aa14c62f58ba448e8cc908eadd1
Jonathan Hodges <johodges@vt.edu>
2024-02-16 20:00:35 -0500

    update rise names again 2

--- scripts/process_rise_database.py
+++ scripts/process_rise_database.py
@@ -398,7 +398,7 @@ if __name__ == "__main__":
             m = material.replace('%',' ').replace('-',' ').replace(',',' ').replace('.',' ')
             while ('  ' in m): m = m.replace('  ',' ')
             m = 'RISE_'+m + '-%0.0f'%(thickness)
-            m = m.replace('|','-')
+            m = m.replace('|','-').replace('handlaminated','laminated')
             
             txt = txt + "\n" + "%s,%s,%s,%s,%s,%s,%s,"%(code, number, 'RISE_Materials', m, matClass, dataFiles, resultDir)
             txt = txt + "%s,%s,%0.0f,%0.8f,%s-%0.0f.csv-Time,%s-%0.0f.csv-HRRPUA,"%(inputFileDir, out_dir_spec, refFlux, thickness, mat_name, refFlux, mat_name, refFlux)
